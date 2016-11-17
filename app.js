e0100f508da5550a32086180a6c5689dc8cee21b
Peter Ilfrich <peterilf@au1.ibm.com>
2016-11-17 14:22:13 +1100

    Added eslint, fixed npm dependencies

--- app.js
+++ app.js
@@ -14,10 +14,11 @@ const HTTP_PORT = process.env.PORT || 3000;
  * NPM imports
  */
 const express = require('express');
-const app = express();
 const fs = require('fs');
 const path = require('path');
 const bodyParser = require('body-parser');
+
+const app = express();
 
 
 /*
@@ -28,7 +29,7 @@ app.use(express.static('client/build'));
 /*
  * Parsing JSON body e.g. for POST calls
  */
-var jsonParser = bodyParser.json()
+const jsonParser = bodyParser.json()
 
 /*
  * Start the app
@@ -41,7 +42,6 @@ app.listen(HTTP_PORT, () => {
 /*
  * Declare GET, POST, DELETE, PUT, etc. endpoints
  */
-
 
 
 /* Example for a post endpoint using the json body parser */
--- webpack.config.js
+++ webpack.config.js
@@ -5,26 +5,26 @@
  *
  */
 
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack');
+const path = require('path');
 
-var BUILD_DIR = path.resolve(__dirname, 'client/public');
-var APP_DIR = path.resolve(__dirname, 'app');
+const BUILD_DIR = path.resolve(__dirname, 'client/build');
+const APP_DIR = path.resolve(__dirname, 'client/src');
 
-var config = {
-    entry: APP_DIR + '/index.js',
+const config = {
+    entry: `${APP_DIR}/index.js`,
     output: {
         path: BUILD_DIR,
-        filename: 'bundle.js'
+        filename: 'bundle.js',
     },
-    module : {
-        loaders : [
+    module: {
+        loaders: [
             {
-                test : /\.jsx?/,
-                include : APP_DIR,
-                loader : 'babel'
-            }
-        ]
+                test: /\.jsx?/,
+                include: APP_DIR,
+                loader: 'babel',
+            },
+        ],
     },
 };
 
