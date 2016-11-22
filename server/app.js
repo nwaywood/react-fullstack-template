92af2ca6a677d5f2e58bc902f7bffd4c46403a9d
Nick Waywood <nwaywood@au1.ibm.com>
2016-11-22 15:10:47 +1100

    cleaned up/reorganised server/app.js

--- server/app.js
+++ server/app.js
@@ -3,45 +3,24 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-/*
- * Loading the environment with default fallbacks
- */
+// Loading the environment port with default fallbacks
 const HTTP_PORT = process.env.PORT || 3000;
 
-
-
-
-
-/*
- * Basic fallback for front-end resources
- */
+// mount static frontend to express
 app.use(express.static('dist'));
 
-/*
- * Parsing JSON body e.g. for POST calls
- */
-const jsonParser = bodyParser.json()
+// mount parser for applicaton/json content
+app.use(bodyParser.json({ limit: '1mb' }));
 
 /*
- * Start the app
+ * API endpoints
  */
+app.get('/api/example', (req, res) => {
+    res.send({ message: 'example GET endpoint' })
+});
+
+
+// Start the app
 app.listen(HTTP_PORT, () => {
     console.log(`Listening on port ${HTTP_PORT}`);
 });
-
-
-/*
- * Declare GET, POST, DELETE, PUT, etc. endpoints
- */
-
-
-/* Example for a post endpoint using the json body parser */
-/*
-app.post('/my/endpoint', jsonParser, (req, res) => {
-    // retrieve request data
-    const data = req.body;
-
-    // return some dummy status
-    res.send({ status: 'OK' });
-});
-*/
