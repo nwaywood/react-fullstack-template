ec7e24dd0d0e23515304f7439651d26e3b57a3c0
Nick Waywood <nwaywood@au1.ibm.com>
2016-11-22 15:10:20 +1100

    removed commented render examples

--- app/src/index.js
+++ app/src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-/* example (import components and then render them into the main document divs) */
-/*
-render(<Header/>, document.getElementById('header-container'));
-render(<Folders/>, document.getElementById('main'));
-*/
 
 render(
   <h1>Hello world</h1>,
