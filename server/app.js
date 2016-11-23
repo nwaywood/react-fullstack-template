226af870990412842dae1a3a2fefabd078321283
Nick Waywood <nwaywood@au1.ibm.com>
2016-11-23 14:36:48 +1100

    Removed semicolons from backend to be consistent with the frontend code

--- server/app.js
+++ server/app.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
 const path = require('path')
 
-const app = express();
+const app = express()
 
 // Loading the environment port with default fallbacks
-const HTTP_PORT = process.env.PORT || 3000;
+const HTTP_PORT = process.env.PORT || 3000
 
 // mount static frontend to express
-app.use(express.static(path.join(__dirname, '..', 'dist')));
+app.use(express.static(path.join(__dirname, '..', 'dist')))
 
 // mount parser for applicaton/json content
-app.use(bodyParser.json({ limit: '1mb' }));
+app.use(bodyParser.json({ limit: '1mb' }))
 
 /*
  * API endpoints
  */
 app.get('/api/example', (req, res) => {
     res.send({ message: 'example GET endpoint' })
-});
+})
 
 // reroute all html (non-api) requests to index.html so browserHistory in react-router works
 app.get('*', (req, res) => {
@@ -28,4 +28,4 @@ app.get('*', (req, res) => {
 // Start the app
 app.listen(HTTP_PORT, () => {
     console.log(`Listening on port ${HTTP_PORT}`);
-});
+})
