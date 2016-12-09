7890f0e67c2aa895a36c72b5adee3a2b6fa2d9f7
Peter Ilfrich <peterilf@au1.ibm.com>
2016-12-09 19:30:04 +1100

    Added support to deliver static fonts, css, images and svgs

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,34 @@ const config = {
                 // config for babel-loader is in .babelrc
                 loaders: ['react-hot-loader/webpack', 'babel'],
             },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader',
+            },
+            {
+                test: /\.png$/,
+                loader: 'url-loader?limit=100000',
+            },
+            {
+                test: /\.jpg$/,
+                loader: 'file-loader',
+            },
+            {
+                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=application/font-woff',
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=application/octet-stream',
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'file',
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=image/svg+xml',
+            },
         ],
     },
     plugins: [
