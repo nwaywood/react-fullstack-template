c039b74dd04e6cb157f516c96c018e003ff3e603
Nick Waywood <nwaywood@au1.ibm.com>
2017-03-27 17:10:15 +1100

    Explain styled-components usage, just incase its confusing

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -7,6 +7,7 @@ const Home = props => (
     </div>
 );
 
+// for each item in posts, map it to a <Li> styled-component containing the title
 const renderItem = ({ title }) => <Li>{title}</Li>;
 
 const Li = styled.li`
