405c9bed79fb6ce089c76cb03fb6a318ec86feb6
Nick Waywood <nwaywood@au1.ibm.com>
2017-03-27 17:01:37 +1100

    prettier-fied server file

--- server/app.js
+++ server/app.js
@@ -1,31 +1,31 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const path = require('path')
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
-const app = express()
+const app = express();
 
 // Loading the environment port with default fallbacks
-const HTTP_PORT = process.env.PORT || 3000
+const HTTP_PORT = process.env.PORT || 3000;
 
 // mount static frontend to express
-app.use(express.static(path.join(__dirname, '..', 'dist')))
+app.use(express.static(path.join(__dirname, "..", "dist")));
 
 // mount parser for applicaton/json content
-app.use(bodyParser.json({ limit: '1mb' }))
+app.use(bodyParser.json({ limit: "1mb" }));
 
 /*
  * API endpoints
  */
-app.get('/api/example', (req, res) => {
-    res.send({ message: 'example GET endpoint' })
-})
+app.get("/api/example", (req, res) => {
+    res.send({ message: "example GET endpoint" });
+});
 
 // reroute all frontend routes to be handled by react-router
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'))
-})
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "dist", "index.html"));
+});
 
 // Start the app
 app.listen(HTTP_PORT, () => {
     console.log(`Listening on port ${HTTP_PORT}`);
-})
+});
