82c88dc8959a4f3bd911fd3a92c376169ccd0600
Nick Waywood <nwaywood@au1.ibm.com>
2017-07-24 14:06:55 +1000

    Added class component flow type

--- app/src/types.js
+++ app/src/types.js
@@ -5,6 +5,8 @@ export type FunctionalComponent<P, Context> = (
     context: Context
 ) => ?React$Element<any>
 
+export type ClassComponent<Def, P, St> = Class<React$Component<Def, P, St>>
+
 export type HOC<Props> = (
     FunctionalComponent<Props, void>
 ) => FunctionalComponent<Props, void>
