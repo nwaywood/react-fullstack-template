6a31e8c02d399e6ac87eb536b5ebe7e39d76141c
Dain Liffman <dainliff@au1.ibm.com>
2017-09-19 11:58:51 +1000

    Fixed fontface Updated some dependencies Fixes #10

--- app/src/globalCSS.js
+++ app/src/globalCSS.js
@@ -1,113 +1,27 @@
-import { injectGlobal } from "emotion/react"
+/* eslint no-unused-expressions:0 */
+import { injectGlobal, fontFace } from "emotion"
+import latoRegular from "../public/assets/fonts/Lato/Lato-Regular.ttf"
+import latoBold from "../public/assets/fonts/Lato/Lato-Bold.ttf"
 
-// eslint-disable-next-line no-unused-expressions
+fontFace`
+  font-family: 'Lato';
+  src: url(${latoRegular});
+  font-weight: 400;
+`
+
+fontFace`
+  font-family: 'Lato';
+  src: url(${latoBold});
+  font-weight: 600;
+`
+
 injectGlobal`
-    @font-face {
-      font-family: 'Lato';
-      src: url('../public/assets/fonts/Lato/Lato-Hairline.ttf');
-      font-weight: 100;
-    }
-    @font-face {
-      font-family: 'Lato';
-      src: url('../public/assets/fonts/Lato/Lato-HairlineItalic.ttf');
-      font-weight: 100;
-      font-style: italic, oblique;
-    }
-    @font-face {
-      font-family: 'Lato';
-      src: url('../public/assets/fonts/Lato/Lato-Light.ttf');
-      font-weight: 200;
-    }
-    @font-face {
-      font-family: 'Lato';
-      src: url('../public/assets/fonts/Lato/Lato-LightItalic.ttf');
-      font-weight: 200;
-      font-style: italic, oblique;
-    }
-    @font-face {
-      font-family: 'Lato';
-      src: url('../public/assets/fonts/Lato/Lato-Light.ttf');
-      font-weight: 300;
-    }
-    @font-face {
-      font-family: 'Lato';
-      src: url('../public/assets/fonts/Lato/Lato-LightItalic.ttf');
-      font-weight: 300;
-      font-style: italic, oblique;
-    }
-    @font-face {
-      font-family: 'Lato';
-      src: url('../public/assets/fonts/Lato/Lato-Regular.ttf');
-      font-weight: 400;
-    }
-    @font-face {
-      font-family: 'Lato';
-      src: url('../public/assets/fonts/Lato/Lato-Italic.ttf');
-      font-weight: 400;
-      font-style: italic, oblique;
-    }
-    @font-face {
-      font-family: 'Lato';
-      src: url('../public/assets/fonts/Lato/Lato-Regular.ttf');
-      font-weight: 500;
-    }
-    @font-face {
-      font-family: 'Lato';
-      src: url('../public/assets/fonts/Lato/Lato-Italic.ttf');
-      font-weight: 500;
-      font-style: italic, oblique;
-    }
-    @font-face {
-      font-family: 'Lato';
-      src: url('../public/assets/fonts/Lato/Lato-Bold.ttf');
-      font-weight: 600;
-    }
-    @font-face {
-      font-family: 'Lato';
-      src: url('../public/assets/fonts/Lato/Lato-BoldItalic.ttf');
-      font-weight: 600;
-      font-style: italic, oblique;
-    }
-    @font-face {
-      font-family: 'Lato';
-      src: url('../public/assets/fonts/Lato/Lato-Bold.ttf');
-      font-weight: 700;
-    }
-    @font-face {
-      font-family: 'Lato';
-      src: url('../public/assets/fonts/Lato/Lato-BoldItalic.ttf');
-      font-weight: 700;
-      font-style: italic, oblique;
-    }
-    @font-face {
-      font-family: 'Lato';
-      src: url('../public/assets/fonts/Lato/Lato-Black.ttf');
-      font-weight: 800;
-    }
-    @font-face {
-      font-family: 'Lato';
-      src: url('../public/assets/fonts/Lato/Lato-BlackItalic.ttf');
-      font-weight: 800;
-      font-style: italic, oblique;
-    }
-    @font-face {
-      font-family: 'Lato';
-      src: url('../public/assets/fonts/Lato/Lato-Black.ttf');
-      font-weight: 900;
-    }
-    @font-face {
-      font-family: 'Lato';
-      src: url('../public/assets/fonts/Lato/Lato-BlackItalic.ttf');
-      font-weight: 900;
-      font-style: italic, oblique;
-    }
-
     * {
         box-sizing: border-box;
     }
 
     body {
-        font-family: "Helvetica Neue", Helvetica, Lato, Arial, "Lucida Grande", sans-serif;
+        font-family: Lato, Arial, "Lucida Grande", sans-serif;
         margin: 0px;
     }
 `
